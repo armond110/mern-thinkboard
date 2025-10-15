32ca646285d0fcc9e4a798101aed89b89d1eb50d
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-10-15 23:01:47 +0200

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
+app.use(express.json()); //this middleware is used to parse JSON bodies
+
+app.use(rateLimiter);
+
+//app.use((req, res, next) => {
+//console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//next();
+//});
+
+app.use('/api/notes', notesRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on PORT:', PORT);
+  });
+});
